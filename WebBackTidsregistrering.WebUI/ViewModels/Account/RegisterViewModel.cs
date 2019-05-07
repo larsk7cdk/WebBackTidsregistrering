7acb973a46e4a90655774846e65a4d036fd3b79b
lars@k7c.dk <lars@k7c.dk>
2019-05-07 20:18:54 +0200

    fixed text

--- WebBackTidsregistrering.WebUI/ViewModels/Account/RegisterViewModel.cs
+++ WebBackTidsregistrering.WebUI/ViewModels/Account/RegisterViewModel.cs
@@ -6,19 +6,19 @@ namespace WebBackTidsregistrering.WebUI.ViewModels.Account
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+        [StringLength(100, ErrorMessage = "The {0} skal være mindst {2} og højst {1} karakterer lang.",
             MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Kodeord")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Skriv kodeord igen")]
+        [Compare("Password", ErrorMessage = "Kodeord stemmer ikke overens.")]
         public string ConfirmPassword { get; set; }
     }
 }
