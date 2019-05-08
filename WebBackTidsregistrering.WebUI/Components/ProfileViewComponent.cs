edbf7dedfabc02b8ca6eaa7a0e9bd9bebea6d297
lars@k7c.dk <lars@k7c.dk>
2019-05-08 22:11:22 +0200

    added view component

---
+++ WebBackTidsregistrering.WebUI/Components/ProfileViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebBackTidsregistrering.WebUI.Components
+{
+    public class ProfileViewComponent : ViewComponent
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ProfileViewComponent(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (_signInManager.IsSignedIn(HttpContext.User))
+            {
+                var model = await _userManager.FindByNameAsync(User.Identity.Name);
+                return View(model);
+            }
+
+            return View(null);
+        }
+    }
+}
